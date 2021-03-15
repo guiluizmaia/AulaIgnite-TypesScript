2a11cc97fe06b7167992c30469083f1b141e68db
Guilherme Luiz <guilhermemaia201450@gmail.com>
2021-03-15 19:11:31 -0300

    Finalizada

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Request, response, Response } from 'express';
+
+import CreateCourseService from './CreateCourseService';
+
+export function createCourse(req: Request, res: Response){
+    CreateCourseService.execute({
+        name: "NodeJS", 
+        duration: 10, 
+        educator: "Dani"
+    });
+
+    CreateCourseService.execute({
+        name: "ReactJS", 
+        educator: "Diego"
+    });
+
+    return res.send();
+}
